755f6c1f3059ebb13e8c08809be6b89f342d1650
Ronen Lapushner <ronenk17@gmail.com>
2022-09-02 11:31:51 +0300

    Added counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
 <script>
   import fetchData from './services/dataService';
   import NameList from './lib/NameList.svelte';
+
+  let counter = 0;
+
+  const incrementCount = () => {
+    counter++;
+  };
 </script>
 
 <main>
   <h1>Name List</h1>
-  {#await fetchData()}
-    <p>LOADING</p>
-  {:then data}
-    <NameList names={data} />
-  {:catch error}
-    <p>ERROR</p>
-  {/await}
+
+  <button on:click={incrementCount}>
+    {counter}
+  </button>
+
+  <div>
+    {#await fetchData()}
+      <p>LOADING</p>
+    {:then data}
+      <NameList names={data} />
+    {:catch error}
+      <p>ERROR</p>
+    {/await}
+  </div>
 </main>
